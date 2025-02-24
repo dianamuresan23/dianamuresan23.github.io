c1d1b1a28d4cfae15bc9d070d811e306e8d3f5de
Diana Muresan <diana.muresan@nexttech.ro>
2025-02-24 17:48:42 +0200

    Eid Impl

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const browserInfo = window.navigator.userAgent;
 document.getElementById("browser-info").innerText = "Browser: " + browserInfo.concat(" ", window.navigator.vendor);
  
 
-if (isMobile) {
-	document.getElementById("dektop_section").hidden = true;
+if (isMobile === true) {
+	document.getElementById("desktop_section").hidden = true;
 	document.getElementById("mobile_section").hidden = false;
 
 	const mobileElem = document.getElementById("mobile_a");
 	AusweisApp2.observeEIDLink(mobileElem, () => {
 		console.log("App is not installed");
 	  
-		document.getElementById("eid-install-app-hint").hidden = false;
+		document.getElementById("eid-install-app-hint").hidden = true;
 	});
 } else {
 	document.getElementById("mobile_section").hidden = true;
@@ -35,7 +35,7 @@ if (isMobile) {
 				document.getElementById("desktop_a").disabled = true;
 			}
 			if (s.includes("safari")) {
-				s = "safari: we cannot detect if you have the app opened or not";
+				s += "- we cannot detect if you have the app opened or not";
 			}
 			document.getElementById("status").innerText = s;
 		}
