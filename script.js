5f578d7eb64dd080753c0372df51f18db66bb547
Diana Muresan <diana.muresan@nexttech.ro>
2025-02-24 13:18:51 +0200

    Eid Impl

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ const observer = new AusweisApp2.StationaryStatusObserver((status) => {
   if (s.includes("safari")) {
 	s = "Status: Please start the AusweisApp2 first";
   }
+  
+  document.getElementById("status").innerText = s;
+  
+  const browserInfo = window.navigator.userAgent;
+  
+  document.getElementById("browser-info").innerText = "Browser: " + browserInfo.concat(" ", window.navigator.vendor);
+  
+  const mobileElem = document.getElementById("mobile_a");
+  AusweisApp2.observeEIDLink(mobileElem, () => {
+			console.log("App nicht installiert?");
+		  
+			document.getElementById("eid-install-app-hint").hidden = false;
+  });
 });
 
 observer.observe();
